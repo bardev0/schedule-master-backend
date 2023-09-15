d22717914198bd83835921f4a40ac492e0943376
GregD <test@gmail.com>
2023-09-15 13:24:34 +0200

    add zod data vaidation

--- src/controlers/findMainUser.ts
+++ src/controlers/findMainUser.ts
@@ -5,5 +5,7 @@ module.exports = async (req: Request, res: Response) => {
     // console.log(req.body);
     // console.log("app");
     const result = await findUser(req.body.id);
-    res.json({ ...result });
+    let reducedData = {...result}
+    reducedData.password = ""
+    res.json(reducedData);
 };
